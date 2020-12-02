2626b962f003c835b8879bb804d950d54ee1fdbe
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-12-02 13:06:27 +0530

    chnage1

--- src/app.js
+++ src/app.js
@@ -3,24 +3,27 @@ const express = require('express');
 const app = express()
 
 
+const Subscriber = require("./models/subscribers");
+
 // Your code goes here
 
+app.get('/subscribers' , async (req,res)=>{
+      
+    let data = [];
+    const sub =  Subscriber.find().then(sub => res.send(sub) );
+    return;
 
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/subscribers/names', (req, res) => {
+  Subscriber.find().select({name: 1,subscribedChannel: 1}).then(subscribers => res.send(subscribers));
+  return;
+});
+app.get('/subscribers/:id', (req, res) => {
+  const id = req.params.id;
+   Subscriber.find({_id : id}).then(sub => res.send(sub) ).catch(error => res.status(400).send({message: error.message}));
+  return;
+});
 
 
 
